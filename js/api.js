03e4ba2af0eb30831dccdb554fd25ca33a8d1e17
nabanichowdhury <nabanichowdhury@gmail.com>
2022-03-01 03:43:18 +0530

    sixth-commit

--- js/api.js
+++ js/api.js
@@ -10,22 +10,28 @@ const searchPhone=()=>{
     const searchField=document.getElementById("search-field")
     const searchError=document.getElementById("search-error")
     const searchText=searchField.value.toLowerCase() ;
+    const div=document.createElement("div");
     if(searchText==""){
-        const div=document.createElement("div");
+        
         div.innerHTML=`
         <h1>Please search a item's name :)</h1>
 
         `
         searchError.appendChild(div)
-        return;
+        
+        
+        
     }
-    searchField.value="";
+    else{
+        searchError.textContent=""
+        searchField.value="";
     // console.log(searchText)  ;
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
     // console.log(url)
     fetch(url)
     .then(res=>res.json())
     .then(queries=>showPhones(queries.data));
+    }
 }
 
 const showPhones=phones=>{
@@ -51,7 +57,7 @@ const showPhones=phones=>{
             <div class="card-body" style="margin-left:50px">
               <h5 class="card-title">${phone.phone_name}</h5>
               <p class="card-text">Brand:${phone.brand}</p>
-              <button href="#"  onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">Go somewhere</button>
+              <button href="#"  onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
           </div>
         `;
@@ -82,7 +88,7 @@ const showPhoneDetails=phoneinfo=>{
         <div class="card-body">
             <h4 class="card-text">Sensors:${phoneinfo.mainFeatures.sensors[0]}, ${phoneinfo.mainFeatures.sensors[1]},${phoneinfo.mainFeatures.sensors[2]},${phoneinfo.mainFeatures.sensors[3]},${phoneinfo.mainFeatures.sensors[4]},${phoneinfo.mainFeatures.sensors[5]}</h4>
             <h5 class="card-text">${pairs} </h5>
-           <h5 class="card-text">Release Date:  ${phoneinfo.releaseDate} </h5>
+           <h5 class="card-text">Release Date:  ${phoneinfo.releaseDate!=''?phoneinfo.releaseDate:"No info found :("} </h5>
 
         </div>
     `
